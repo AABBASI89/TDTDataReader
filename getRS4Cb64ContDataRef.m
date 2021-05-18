a2a9115cff4045ece119367c34ecd73820e084d8
AamirAbbasi <aamirabbasi89@gmail.com>
2021-05-18 12:05:05 -0700

    no message

--- getRS4Cb64ContDataRef.m
+++ getRS4Cb64ContDataRef.m
@@ -20,44 +20,60 @@ polytrode_grps = [[21 17 53 18 57 22 23 30 55 61 32 63 59 19 20 28];
 start = tic;
 for j=1:length(blocks)
   blockNames = dir([root,blocks{j}]);
-  parfor i = 1:length(blockNames)
+  for i = 1:length(blockNames)
     blockpath = [root,blockNames(i).name,'\'];
     disp(blockpath);
     
-    for ch = 1:totChannels_m1
+    parfor ch = 1:totChannels_m1
       
-      % disp(['reading M1 channel ', num2str(ch-1)]);
+      disp(['reading M1 channel ', num2str(ch-1)]);
       
       % Read M1 raw data for a single channel from RS4 .sev files
       raw_M1 = SEV2mat(blockpath,'CHANNEL',ch);
       
       % Extract M1 single units continous data
-      su_M1 = raw_M1.RSn1.data;
-      
-      % Make save directory if it dosen't exist
-      currentpath = [savepath,blockNames(i).name(1:11),'_DAT_files\M1\Channel_',num2str(ch-1),'\'];
-      if ~exist(currentpath,'dir')
-        mkdir(currentpath);
-      end
-
-      % Save continous M1 data to a binary file
-      su_M1 = su_M1(:)';
-      fileID = fopen([currentpath,'SU_CONT_M1_Ch_',num2str(ch-1),'_',num2str(i-1),'.dat'],'w');
-      fwrite(fileID,su_M1,'float32');
-      fclose(fileID);
-      
+      su_M1(ch,:) = raw_M1.RSn1.data;
+         
     end
     
-    for ch = 1:totPolytrodes
+    % Reject common mean reference
+    su_M1 = su_M1 - mean(su_M1);
+    
+    % Save channel by channel data in .DAT files
+    for ch = 1:size(su_M1,1)
+        % Make save directory if it dosen't exist
+        currentpath = [savepath,blockNames(i).name(1:11),'_DAT_files\M1\Channel_',num2str(ch-1),'\'];
+        if ~exist(currentpath,'dir')
+            mkdir(currentpath);
+        end        
+        % Save continous M1 data to a binary file
+        M1_DAT = su_M1(ch);
+        fileID = fopen([currentpath,'SU_CONT_M1_Ch_',num2str(ch-1),'_',num2str(i-1),'.dat'],'w');
+        fwrite(fileID,M1_DAT,'float32');
+        fclose(fileID);
+    end
+    
+    parfor ch = 1:length(polytrode_grps(:))
       
-      % disp(['reading Cb tetrode ', num2str(tet-1)]);
+      disp(['reading Cb channel ', num2str(ch-1)]);
       
       % Read Cb raw data from RS4 .sev files (Cb channels are saved as ch 33-96 by RS4)
-      raw_Cb = SEV2mat(blockpath,'CHANNEL', polytrode_grps(ch)+32);
+      raw_Cb = SEV2mat(blockpath,'CHANNEL', ch+32);
       
       % Extract Cb single units continous data
-      su_Cb = raw_Cb.RSn1.data;
+      su_Cb(ch,:) = raw_Cb.RSn1.data;
+     
+    end
+    
+    % Reject common mean reference
+    su_Cb = su_Cb - mean(su_Cb);
+    
+    % Save channel by channel data in .DAT files
+    for ch = 1:totPolytrodes
       
+      % Get channels for a single polytrode
+      cb_DAT = su_Cb(polytrode_grps(ch),:);
+        
       % Make save directory if it dosen't exist 
       currentpath = [savepath,blockNames(i).name(1:11),'_DAT_files\Cb\Polytrode_',num2str(ch-1),'\'];
       if ~exist(currentpath, 'dir')
@@ -65,12 +81,12 @@ for j=1:length(blocks)
       end
 
       % Save continous Cb data to a binary file
-      su_Cb = su_Cb(:)';
+      cb_DAT = cb_DAT(:)';
       fileID = fopen([currentpath,'SU_CONT_Cb_poly_',num2str(ch-1),'_',num2str(i-1),'.dat'],'w');
-      fwrite(fileID,su_Cb,'float32');
+      fwrite(fileID,cb_DAT,'float32');
       fclose(fileID);
+    end    
     
-    end
   end
 end
 runTime = toc(start);
